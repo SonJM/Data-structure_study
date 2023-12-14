e8e143373ba9128e020f00da347d1542499853a2
Son Jeong Min <blisle@naver.com>
2023-12-14 14:22:11 +0900

    [Silver I] Title: 나이트의 이동, Time: 300 ms, Memory: 72172 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,63 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.awt.Point;
 
 public class Main {
-    static int T, I;
-    static int[][] map, count;
-    static int[] dx = {-1,-2,-2,-1,1,2,2,1};
-    static int[] dy = {2,1,-1,-2,-2,-1,1,2};
+    static int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+    static int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+    static int mapSize;
+    static int[][] ch;
+    static List<Integer> result = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        StringBuilder result = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        T = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(bf.readLine());
+        int t = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < t; i++) {
+            st = new StringTokenizer(bf.readLine()); //체스판 한 변의 길이 입력
+            mapSize = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<T; i++){
-            I = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(bf.readLine()); //나이트가 현재 있는 칸
+            int start_x = Integer.parseInt(st.nextToken());
+            int start_y = Integer.parseInt(st.nextToken());
 
-            map = new int[I][I];
-            count = new int[I][I];
-            st = new StringTokenizer(br.readLine());
-            int start_H = Integer.parseInt(st.nextToken());
-            int start_W = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(bf.readLine()); //나이트가 이동하려는 칸
+            int end_x = Integer.parseInt(st.nextToken());
+            int end_y = Integer.parseInt(st.nextToken());
 
-            st = new StringTokenizer(br.readLine());
-            int end_H = Integer.parseInt(st.nextToken());
-            int end_W = Integer.parseInt(st.nextToken());
-
-            bfs(start_H, start_W, end_H, end_W);
-            result.append(count[end_H][end_W]);
-            result.append('\n');
+            BFS(start_x, start_y, end_x, end_y);
         }
-        System.out.println(result);
+        for(int x : result) System.out.println(x);
     }
-
-    private static void bfs(int start_H, int start_W, int end_H, int end_W){
-        Queue queue = new LinkedList<Point>();
-        queue.offer(new Point(start_H, start_W));
-        count[start_H][start_W] = 0;
-
-        while(!queue.isEmpty()){
-            Point p = (Point)queue.poll();
-            if(p.x == end_H && p.y == end_W) break;
-
-            for(int i=0; i<8; i++){
-                int next_H = p.x + dy[i];
-                int next_W = p.y + dx[i];
-
-                if(next_H>=0 && next_H<I && next_W>=0 && next_W<I && map[next_H][next_W]==0){
-                    map[next_H][next_W] = 1;
-                    queue.offer(new Point(next_H,next_W));
-                    count[next_H][next_W] = count[p.x][p.y] + 1;
+    public static void BFS(int s_x, int s_y, int e_x, int e_y){
+        ch = new int[mapSize][mapSize];
+        ch[s_x][s_y]=1;
+        Queue<Point> Q = new LinkedList<>();
+        Q.add(new Point(s_x, s_y));
+        int level=0;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for(int i=0; i<len; i++) {
+                Point cur = Q.poll();
+                if(cur.x == e_x && cur.y == e_y) {
+                    result.add(level);
+                    return;
+                }
+                for (int j = 0; j < 8; j++) {
+                    int nx = cur.x + dx[j];
+                    int ny = cur.y + dy[j];
+                    if(nx>=0 && nx<mapSize && ny>=0 && ny<mapSize && ch[nx][ny]==0) {
+                        ch[nx][ny]=1;
+                        Q.add(new Point(nx, ny));
+                    }
                 }
             }
+            level++;
         }
-
-
     }
 }
