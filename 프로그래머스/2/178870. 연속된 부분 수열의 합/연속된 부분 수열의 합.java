75b4195fdfd919f656e681ee2a756d4f6ba3fc31
Son Jeong Min <blisle@naver.com>
2024-01-09 16:06:13 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.29 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,36 @@
+import java.awt.Point;
+import java.util.*;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+            int[] answer = new int[2];
+            List<Point> list = new ArrayList<>();
+            Point p = new Point(0,0);
+            int sum = sequence[0];
+            while (true) {
+                if (sum == k) {
+                    list.add(new Point(p.x, p.y));
+                    sum -= sequence[p.x];
+                    p.x++;
+                } else if (sum > k) {
+                    sum -= sequence[p.x];
+                    p.x++;
+                } else {
+                    p.y++;
+                    if(p.y == sequence.length) break;
+                    sum += sequence[p.y];
+                }
+            }
+            int min = Integer.MAX_VALUE;
+            for (Point point : list) {
+                int diff = point.y - point.x;
+                if (min > diff) {
+                    answer[0] = point.x;
+                    answer[1] = point.y;
+                    min = diff;
+                }
+            }
+            return answer;
+    }
+}
