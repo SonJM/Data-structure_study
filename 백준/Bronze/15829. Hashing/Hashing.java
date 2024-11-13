3684b6c241a39da671438fec78b30885ea81b59e
Son Jeong Min <blisle@naver.com>
2024-11-13 13:52:28 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14364 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,26 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
+        int L = Integer.parseInt(br.readLine());
+        String str = br.readLine();
 
-        st = new StringTokenizer(br.readLine());
-        String str = st.nextToken();
-        for(int i=0; i<N; i++){
-            arr[i] = str.charAt(i) - '`';
+        long result = 0;
+        long pow = 1;
+        long mod = 1234567891;
+
+        for(int i = 0; i < L; i++) {
+            result = (result + ((str.charAt(i) - 'a' + 1) * pow)) % mod;
+            pow = (pow * 31) % mod;
         }
-
-        int sum = 0;
-        for(int i=0; i< arr.length; i++){
-            sum += (arr[i] * (int)Math.pow(31, i));
-        }
-        System.out.println(sum);
+        System.out.println(result);
     }
 }
