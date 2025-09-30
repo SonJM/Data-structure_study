08a48a1aa94f6f52a3d2d312015e248770fdee0c
Son Jeong Min <blisle@naver.com>
2025-09-30 09:51:34 +0900

    [Silver V] Title: 집합, Time: 1044 ms, Memory: 312836 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,59 +2,54 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * 효율적인 해결책: 비트마스킹
+ * 연산	비트마스킹 연산	설명
+ * add x	S |= (1 << x)	x번째 비트를 1로 만듭니다. (OR 연산)
+ * remove x	S &= ~(1 << x)	x번째 비트를 0으로 만듭니다. (AND와 NOT 연산)
+ * check x	(S & (1 << x)) != 0	x번째 비트가 1인지 확인합니다. (AND 연산)
+ * toggle x	S ^= (1 << x)	x번째 비트를 반전시킵니다. (XOR 연산)
+ * all	S = (1 << 21) - 1	1~20번째 비트를 모두 1로 만듭니다.
+ * empty	S = 0	모든 비트를 0으로 만들어 비웁니다.
+  */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		List<Integer> list = new LinkedList<>();
 
 		StringTokenizer st;
 		int M = Integer.parseInt(br.readLine());
+		int S = 0;
 		while(M-- > 0) {
 			st = new StringTokenizer(br.readLine());
-
 			String cmd = st.nextToken();
+			int num;
 
-			if(cmd.equals("all")) {
-				list = new LinkedList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20));
-			} else if(cmd.equals("empty")) {
-				list = new LinkedList<>();
-			} else {
-				int num = Integer.parseInt(st.nextToken());
-				switch (cmd) {
-					case "add":
-						list.add(num);
-						break;
-					case "check":
-					{
-						if (list.contains(num)) {
-							sb.append(1).append("\n");
-						} else {
-							sb.append(0).append("\n");
-						}
-						break;
-					}
-					case "remove":
-					{
-						for(int i=0; i<list.size(); i++) {
-							if(list.get(i) == num) list.remove(i);
-						}
-						break;
-					}
-					case "toggle":
-					{
-						boolean flag = false;
-						for(int i=0; i<list.size(); i++) {
-							if(list.get(i) == num) {
-								list.remove(i);
-								flag = true;
-							}
-						}
-						if(!flag) list.add(num);
-						break;
-					}
-				}
+			switch (cmd) {
+				case "add":
+					num = Integer.parseInt(st.nextToken());
+					S |= (1 << num);
+					break;
+				case "remove":
+					num = Integer.parseInt(st.nextToken());
+					S &= ~(1 << num);
+					break;
+				case "check":
+					num = Integer.parseInt(st.nextToken());
+					sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+					break;
+				case "toggle":
+					num = Integer.parseInt(st.nextToken());
+					S ^= (1 << num);
+					break;
+				case "all":
+					S = (1 << 21) - 1;
+					break;
+				case "empty":
+					S = 0;
+					break;
 			}
+
 		}
 		System.out.println(sb);
 	}
