2e4c514059ae2e55f6d7db3ccc558888052cc43d
Son Jeong Min <blisle@naver.com>
2025-10-16 09:16:50 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2324 ms, Memory: 356276 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -9,40 +9,37 @@ public class Main {
 
 		int T = Integer.parseInt(br.readLine());
 		while(T-- > 0) {
-			TreeMap<Integer, Integer> queue = new TreeMap<>();
-			int k = Integer.parseInt(br.readLine());
-			StringTokenizer st;
-			for(int i=0; i<k; i++) {
-				st = new StringTokenizer(br.readLine());
-				String cmd = st.nextToken();
-				int num = Integer.parseInt(st.nextToken());
-				if(cmd.equals("I")) {
-					queue.put(num, queue.getOrDefault(num, 0) + 1);
-				} else if (cmd.equals("D")) {
-					if(!queue.isEmpty()) {
-						if(num == 1) {
-							Map.Entry<Integer, Integer> entry = queue.pollLastEntry();
-							int key = entry.getKey();
-							int value = entry.getValue() - 1;
-							if(value > 0) queue.put(key, value);
-						} else if(num == -1) {
-							Map.Entry<Integer, Integer> entry = queue.pollFirstEntry();
-							int key = entry.getKey();
-							int value = entry.getValue() - 1;
-							if(value > 0) queue.put(key, value);
-						}
-					}
-				}
-			}
-			if(queue.isEmpty()) {
-				sb.append("EMPTY").append("\n");
-			} else if(queue.size() == 1) {
-				int num = queue.pollFirstEntry().getKey();
-				sb.append(num).append(" ").append(num).append("\n");
-			} else {
-				sb.append(queue.pollLastEntry().getKey()).append(" ").append(queue.pollFirstEntry().getKey()).append("\n");
-			}
+			solve(br, sb);
 		}
 		System.out.println(sb);
+	}
+	public static void solve(BufferedReader br, StringBuilder sb) throws Exception {
+		TreeMap<Integer, Integer> queue = new TreeMap<>();
+		int k = Integer.parseInt(br.readLine());
+		for(int i = 0; i < k; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String cmd = st.nextToken();
+			int num = Integer.parseInt(st.nextToken());
+
+			if(cmd.equals("I")) {
+				queue.put(num, queue.getOrDefault(num, 0) + 1);
+			} else {
+				if(queue.isEmpty()) continue;
+
+				int key = (num == 1) ? queue.lastKey() : queue.firstKey();
+				int count = queue.get(key);
+				if(count == 1) {
+					queue.remove(key);
+				} else {
+					queue.put(key, count - 1);
+				}
+			}
+		}
+
+		if(queue.isEmpty()) {
+			sb.append("EMPTY").append("\n");
+		} else {
+			sb.append(queue.lastKey()).append(" ").append(queue.firstKey()).append("\n");
+		}
 	}
 }
